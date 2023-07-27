a5feef902404ed429232dada78aaa0e723ef63af
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-27 02:42:28 -0700

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """
+        pascal triangle function taking
+    """
+    if n <= 0:
+        return []
+
+    pascal = [[1]]
+    for i in range(1, n + 1):
+        test = []
+        if i == 1:
+            pass
+        else:
+            for j in range(0, i):
+                if j == 0 or j == i - 1:
+                    test.append(1)
+                else:
+                    test.append(test2[j] + test2[j-1])
+            test2 = test
+            pascal.append(test)
+    return pascal
